5bfc62c14df0524ff89cb69d4a6c9410c4f9c606
Saynam221b <saynam1101@gmail.com>
2022-10-11 10:35:03 +0530

    commit for sum

---
+++ Sum/src/Sum.java
@@ -0,0 +1,23 @@
+
+public class Sum {
+
+	
+	
+		int sum(int a, int b) {
+		
+			
+			return a+b;	
+			
+		}
+		
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		Sum s = new Sum();
+		System.out.println("Sum of 2 no is "+s.sum(2, 3));
+	}
+
+}
